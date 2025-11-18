62c1d99be171a8ac520acf8c63f4107947a642dd
niggbpyoy <growthoh@outlook.com>
2025-11-18 12:06:35 +0100

    update url

--- src/utilities/baseURL.ts
+++ src/utilities/baseURL.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const BASE_URL = import.meta.env.PROD
-  ? `${import.meta.env.VITE_BACKENDURL}/api/v1`
+  ? import.meta.env.VITE_BACKENDURL 
+    ? `${import.meta.env.VITE_BACKENDURL}/api/v1`
+    : "http://localhost:5000/api/v1"
   : "/api/v1"; // localhost dev fallback
 
 export const clientURL = import.meta.env.PROD ? "" : "http://localhost:3000";
